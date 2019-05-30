09cefa2d26f929b4745846a1348270cf6c7d6bdb
Francisco Dias <franciscopdneto@gmail.com>
2019-05-30 00:08:02 -0300

    Adding baseRepos to train and validation data.

---
+++ collectIssues.py
@@ -0,0 +1,19 @@
+from github import Github
+from github import Label
+import json
+
+# using an access token
+g = Github("6085d91cefe7a650d320b2caad2f7eb2fabe82f0")
+
+bugIssues = []
+featureIssues = []
+with open('baseRepos.json') as baseReposFile:
+    baseRepos = json.load(baseReposFile)
+    for language, repoNames in baseRepos.items():
+        for repoAndLabelIssuesName in repoNames:
+            repoName = list(repoAndLabelIssuesName.keys())[0]
+            repo = g.get_repo(repoName)
+            for labelName in list(repoAndLabelIssuesName.values())[0]:
+                label = repo.get_label(labelName)
+                issues = repo.get_issues(labels=[label])
+                print(issues)
