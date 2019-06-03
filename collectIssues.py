c2e9860abe40f1e90a5b6330dd1b3a7772c1f90e
Francisco Dias <franciscopdneto@gmail.com>
2019-06-03 15:27:27 -0300

    Getting all bug issues and features to train.

--- collectIssues.py
+++ collectIssues.py
@@ -13,7 +13,16 @@ with open('baseRepos.json') as baseReposFile:
         for repoAndLabelIssuesName in repoNames:
             repoName = list(repoAndLabelIssuesName.keys())[0]
             repo = g.get_repo(repoName)
+            isBugIssues = True
             for labelName in list(repoAndLabelIssuesName.values())[0]:
                 label = repo.get_label(labelName)
                 issues = repo.get_issues(labels=[label])
-                print(issues)
+                for issue in issues:
+                    if isBugIssues:
+                        bugIssues.append(issue)
+                    else:
+                        featureIssues.append(issue)
+                isBugIssues = not isBugIssues
+    
+    print(len(bugIssues))
+    print(len(featureIssues))
